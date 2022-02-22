1aa12156d53955325f11be33311840ae066e33b5
vgj24 <vrushlijoshi24@gmail.com>
2022-02-22 11:20:47 +0530

    Reterive required data from table

--- Payroll_seviceSQLQuery1.sql
+++ Payroll_seviceSQLQuery1.sql
@@ -18,7 +18,22 @@ insert into employee_payroll values
 ('Ramesh',16345.90,'2019-02-07'),
 ('Hari',14312.56,'2022-02-16'),
 ('Prakash',9000.0,'2021-11-28'),
-('Priya',15000,'2022-03-11')
+('Priya',15000,'2022-03-01')
 
---Retetive data from table using Select query
+insert into employee_payroll values
+('Neha',10000,'2022-03-01'),
+('Shriya',20000,'2022-03-01')
+
+
+--UC4 Retetive data from table using Select query
 select *from employee_payroll
+
+--UC5 Ability to retrieve salary data for a particular employee as well as all employees who have
+--joined in a particular data range from the payroll service database
+
+--reteriving person name using where clause
+select salary as 'payment' from employee_payroll where name='Ramesh'
+--reteriving data within range
+select *from employee_payroll where start between '2020-01-01' and getdate()
+--using cast to getdate
+select cast(getdate() as varchar(50))
