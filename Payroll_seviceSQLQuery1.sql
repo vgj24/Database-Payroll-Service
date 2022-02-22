75cff13803bfd85037a7cce926cbdf4e3cbc177f
vgj24 <vrushlijoshi24@gmail.com>
2022-02-22 10:51:55 +0530

    create table

--- Payroll_seviceSQLQuery1.sql
+++ Payroll_seviceSQLQuery1.sql
@@ -1,3 +1,15 @@
 
 --UC1to create a payroll service database
 use payroll_service_new
+
+--UC2 Ability to create a employee payroll table in the payroll service database to manage employee payrolls
+create table employee_payroll
+(
+id int primary key identity,
+name varchar(50) not null,
+salary money not null,
+start date not null
+)
+
+select *from employee_payroll
+
